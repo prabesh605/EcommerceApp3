9c8a680249e52a71afd9c5c68f5a7cbde2b687eb
Prabesh Bhattarai <prabesh605@gmail.com>
2026-08-02 09:30:26 +0545

    notification issues

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled =true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -41,7 +42,9 @@ android {
         }
     }
 }
-
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+}
 flutter {
     source = "../.."
 }
